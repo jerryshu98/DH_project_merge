ebb74149d068885651a440031403fbee76b9316d
jerryshu98 <jerryshu98@gmail.com>
2023-11-14 11:25:59 +0800

    first commit

---
+++ data_loader.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import math
+
+data_basic = './data/gender_race_admission_type_charttime_fio2_plateaupressure.csv'
+data_basic2 = './data/basic_data2.csv'
+data_O2 = './data/O2_flow.csv'
+data_vet = './data/ventailation_data.csv'
+
+data_basic_datalist = pd.read_csv(data_basic)
+data_basic2_datalist = pd.read_csv(data_basic2)
+data_O2_datalist = pd.read_csv(data_O2)
+data_vet_datalist = pd.read_csv(data_vet)
+
+df = data_vet_datalist
+df['charttime'] = pd.to_datetime(df['charttime'])
+df = df.sort_values(by=['stay_id', 'charttime'])
+df = df.set_index('charttime')
+df_resampled = df.groupby('stay_id').resample('H').max()
+df_resampled['respiratory_rate_set'] = df_resampled['respiratory_rate_set'].fillna(method='ffill')
+df_resampled['tidal_volume_observed'] = df_resampled['tidal_volume_observed'].fillna(method='ffill')
+df_resampled['peep'] = df_resampled['peep'].fillna(method='ffill')
+df_resampled['minute_ventilation'] = df_resampled['minute_ventilation'].fillna(method='ffill')
+df_resampled = df_resampled.drop(columns='stay_id')
+df_resampled = df_resampled.reset_index()
+df_resampled['subject_id'] = df_resampled.groupby('stay_id')['subject_id'].fillna(method='ffill')
+
+
+df1 = data_O2_datalist
+df1['charttime'] = pd.to_datetime(df1['charttime'])
+df1 = df1.sort_values(by=['stay_id', 'charttime'])
+df1 = df1.set_index('charttime')
+df_resampled1 = df1.groupby('stay_id').resample('H').max()
+df_resampled1['O2_flow'] = df_resampled1['O2_flow'].fillna(method='ffill')
+df_resampled1 = df_resampled1.drop(columns='stay_id')
+df_resampled1 = df_resampled1.reset_index()
+df_resampled1['subject_id'] = df_resampled1.groupby('stay_id')['subject_id'].fillna(method='ffill')
+
+df2 = data_basic_datalist
+df2['charttime'] = pd.to_datetime(df2['charttime'])
+df2 = df2.sort_values(by=['stay_id', 'charttime'])
+df2 = df2.set_index('charttime')
+df_resampled2 = df2.groupby('stay_id').resample('H').max()
+df_resampled2['gender'] = df_resampled2['gender'].fillna(method='ffill')
+df_resampled2['race'] = df_resampled2['race'].fillna(method='ffill')
+df_resampled2['admission_type'] = df_resampled2['admission_type'].fillna(method='ffill')
+df_resampled2['fio2'] = df_resampled2['fio2'].fillna(method='ffill')
+df_resampled2['plateau_pressure'] = df_resampled2['plateau_pressure'].fillna(method='ffill')
+df_resampled2 = df_resampled2.drop(columns='stay_id')
+df_resampled2 = df_resampled2.drop(columns='hadm_id')
+df_resampled2 = df_resampled2.reset_index()
+df_resampled2['subject_id'] = df_resampled2.groupby('stay_id')['subject_id'].fillna(method='ffill')
+
+df3 = data_basic2_datalist
+df_merged = pd.merge(df_resampled2, df3, on=['stay_id', 'subject_id'], how='inner')
+df_merged = pd.merge(df_merged, df_resampled1, on=['stay_id', 'subject_id', 'charttime'], how='inner')
+df_merged = pd.merge(df_merged, df_resampled, on=['stay_id', 'subject_id', 'charttime'], how='inner')
+df_temp = df_merged
+df_merged = df_merged.drop(columns=['subject_id'])
+df_merged.insert(0, 'subject_id', df_temp['subject_id'])
+df_merged['RSBI'] = df_merged['tidal_volume_observed']/df_merged['respiratory_rate_set']
+
+merge_data = './data/merged_data.csv'
+df_merged.to_csv(merge_data, index=False)
---
+++ data_viewer.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import math
+stay_id_to_find = 30005707
+if len(sys.argv) >= 2:
+    stay_id_to_find = int(sys.argv[1])
+data_merge = './data/merged_data.csv'
+data_merge_datalist = pd.read_csv(data_merge)
+stay_id_data = data_merge_datalist[data_merge_datalist['stay_id'] == stay_id_to_find]
+df_str = stay_id_data.to_string(index=False)
+for line in df_str.split('\n'):
+    print(line)
